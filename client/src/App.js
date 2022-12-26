9c5224585ce33fdc28ca6932e922ddd7efcbb462
DongMinKwon <38908169+DongMinKwon@users.noreply.github.com>
2022-12-26 15:14:14 +0900

    react initial setting (#22)
    * react tailwindcss setting
    
    * react init path config, folder, prettier setting

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
+        HelloWorld!
+      </p>
+    </div>
+  );
+}
+
+export default App;

9d9f73ef4f44e1cf32ffd348412fb58839dd3030
DongMinKwon <wt0630@naver.com>
2022-12-26 14:29:36 +0900

    react init path config, folder, prettier setting

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <p className="underline mx-10">HelloWorld!</p>
+      <p className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
+        HelloWorld!
+      </p>
     </div>
   );
 }

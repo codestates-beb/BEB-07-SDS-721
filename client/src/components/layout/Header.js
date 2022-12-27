282359bf37aa46197d5aceae846ccc17a19a72b8
DongMinKwon <wt0630@naver.com>
2022-12-27 09:23:13 +0900

    nft card add

--- client/src/components/layout/Header.js
+++ client/src/components/layout/Header.js
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';
 const Header = () => {
   return (
     <div className="header bg-blue">
-      <div className="header-Content relative mx-auto h-20 w-4/5">
-        <div className="logo absolute inset-y-0 my-auto h-[57px] text-[40px] font-semibold text-white">
+      <div className="header-inner relative mx-auto h-20 w-5/6">
+        <div className="logo absolute inset-y-0 my-auto h-[57px] text-[40px] text-white">
           Opensea
         </div>
         <ul className="category absolute inset-y-0 right-0 my-auto flex h-[60px] items-center text-xl text-white">
@@ -19,7 +19,7 @@ const Header = () => {
             <li>MyPage</li>
           </Link>
           <li className="h-[100%]">
-            <button className="mr-[10px] flex h-[100%] w-[140px] items-center justify-center rounded-lg bg-blue-light font-semibold hover:cursor-pointer hover:bg-white hover:text-blue-light">
+            <button className="mr-[10px] flex h-[100%] w-[140px] items-center justify-center rounded-lg bg-blue-light hover:cursor-pointer hover:bg-white hover:text-blue-light">
               Log In
             </button>
           </li>
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,53 @@
 import './Home.css';
+import NftCard from 'components/features/NftCard';
+
+import sample1 from 'img/card_sample_1.jpg';
+import sample2 from 'img/card_sample_2.jpg';
+import sample3 from 'img/card_sample_3.jpg';
+import sample4 from 'img/card_sample_4.jpg';
 
 const Home = () => {
   return (
-    <div className="home h-screen bg-gray-light">
-      <p className="text-2xl font-bold">This is Home!</p>
+    <div className="home h-screen ">
+      <div className="home-inner mx-auto mt-[19px] w-4/5">
+        <p className="mx-auto mb-8 flex h-[84px] w-[876px] items-center justify-center text-6xl font-semibold">
+          Explore, collect, and sell NFTs
+        </p>
+        <div className="flex h-[387px] ">
+          <div className="mr-[50px] flex-1">
+            <NftCard
+              nft_img={sample1}
+              nft_name="NFT Name"
+              artist_name="NFT Artist"
+              price="1.63"
+            />
+          </div>
+          <div className="mr-[50px] flex-1">
+            <NftCard
+              nft_img={sample2}
+              nft_name="NFT Name"
+              artist_name="NFT Artist"
+              price="1.63"
+            />
+          </div>
+          <div className="mr-[50px] flex-1">
+            <NftCard
+              nft_img={sample3}
+              nft_name="NFT Name"
+              artist_name="NFT Artist"
+              price="1.63"
+            />
+          </div>
+          <div className="flex-1">
+            <NftCard
+              nft_img={sample4}
+              nft_name="NFT Name"
+              artist_name="NFT Artist"
+              price="1.63"
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };

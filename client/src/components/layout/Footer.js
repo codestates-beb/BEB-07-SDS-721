4ced78943c07b0d857a7b1094d879d69ab7c9df5
NotoriousHong <hjs97040@gmail.com>
2022-12-27 16:48:34 +0900

    mypage

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -1,7 +1,7 @@
 import './Footer.css';
 
 const Footer = () => {
-  return <div className="footer bg-blue h-1/6">This is footer!</div>;
+  return <div className="footer h-1/6 bg-blue">This is footer!</div>;
 };
 
 export default Footer;

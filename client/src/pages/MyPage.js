d549ce6d1409a768f36938c84e33e260401ff1bd
DongMinKwon <wt0630@naver.com>
2022-12-27 19:53:21 +0900

    mypage grid fix

--- client/src/pages/MyPage.js
+++ client/src/pages/MyPage.js
@@ -22,69 +22,71 @@ const MyPage = () => {
         />
       </figure>
 
-      <div className="ProfileInfo ml-[10em] mt-[15rem]">
-        <div className="text-6xl">Atoye</div>
-        <div className="mt-10">
-          <div className="text-3xl text-gray">Bio</div>
-          <div>The Blockchain's Creative Engineer</div>
+      <div className="mypage-inner ml-[10em]">
+        <div className="ProfileInfo mt-[15rem]">
+          <div className="text-6xl">Atoye</div>
+          <div className="mt-10">
+            <div className="text-3xl text-gray">Bio</div>
+            <div>The Blockchain's Creative Engineer</div>
+          </div>
+          <div className="mt-10">
+            <div className="text-3xl text-gray">Account</div>
+            <div>0x247b669cbdd58fca982dbf337c5d94880852e3fa</div>
+          </div>
         </div>
-        <div className="mt-10">
-          <div className="text-3xl text-gray">Account</div>
-          <div>0x247b669cbdd58fca982dbf337c5d94880852e3fa</div>
-        </div>
-      </div>
 
-      <div className="ml-[10em] mt-[10rem] h-7 w-28 rounded-md border-2 bg-gray text-center">
-        Collected
-      </div>
+        <div className="mt-[10rem] h-7 w-28 rounded-md border-2 bg-gray-light text-center">
+          Collected
+        </div>
 
-      <div className=" ml-[10em] flex flex-wrap">
-        <div className="mr-[5rem] mt-[3rem]">
-          <NftCard
-            nft_img={sample1}
-            nft_name="NFT Name"
-            artist_name="NFT Artist"
-            price="1.63"
-          />
+        <div className="mt-[3rem] grid grid-cols-fill-25 justify-start">
+          <div className="">
+            <NftCard
+              nft_img={sample1}
+              nft_name="NFT Name"
+              artist_name="NFT Artist"
+              price="1.63"
+            />
+          </div>
         </div>
-      </div>
 
-      <div className="ml-[10em] mt-[10rem] h-7 w-28 rounded-md border-2 bg-gray text-center">
-        Created
-      </div>
+        <div className="mt-[10rem] h-7 w-28 rounded-md border-2 bg-gray-light text-center">
+          Created
+        </div>
 
-      <div className=" ml-[10em] flex flex-wrap">
-        <div className="mr-[5rem] mt-[3rem]">
-          <NftCard
-            nft_img={sample1}
-            nft_name="NFT Name"
-            artist_name="NFT Artist"
-            price="1.63"
-          />
-        </div>
-        <div className="mr-[5rem] mt-[3rem]">
-          <NftCard
-            nft_img={sample1}
-            nft_name="NFT Name"
-            artist_name="NFT Artist"
-            price="1.63"
-          />
-        </div>
-        <div className="mr-[5rem] mt-[3rem]">
-          <NftCard
-            nft_img={sample1}
-            nft_name="NFT Name"
-            artist_name="NFT Artist"
-            price="1.63"
-          />
-        </div>
-        <div className="mr-[5rem] mt-[3rem]">
-          <NftCard
-            nft_img={sample1}
-            nft_name="NFT Name"
-            artist_name="NFT Artist"
-            price="1.63"
-          />
+        <div className="mt-[3rem] grid grid-cols-fill-25 justify-start gap-y-12">
+          <div className="">
+            <NftCard
+              nft_img={sample1}
+              nft_name="NFT Name"
+              artist_name="NFT Artist"
+              price="1.63"
+            />
+          </div>
+          <div className="">
+            <NftCard
+              nft_img={sample1}
+              nft_name="NFT Name"
+              artist_name="NFT Artist"
+              price="1.63"
+            />
+          </div>
+          <div className="">
+            <NftCard
+              nft_img={sample1}
+              nft_name="NFT Name"
+              artist_name="NFT Artist"
+              price="1.63"
+            />
+          </div>
+          <div className="">
+            <NftCard
+              nft_img={sample1}
+              nft_name="NFT Name"
+              artist_name="NFT Artist"
+              price="1.63"
+            />
+          </div>
         </div>
       </div>
     </div>

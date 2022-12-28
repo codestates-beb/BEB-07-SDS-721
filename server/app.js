a62d53f56ae6da5084fda31fc0acee6184f5b558
SeolDonghun <ssalssi1@gmail.com>
2022-12-28 10:25:58 +0900

    Feature: transaction event listener, edit web3 index.js file with it

--- server/app.js
+++ server/app.js
@@ -6,13 +6,14 @@ const morgan = require('morgan');
 const ejs = require('ejs');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
+
 const connect = require('./schemas');
 
+connect();
 const devRouter = require('./routes/dev');
 const nftsRouter = require('./routes/nfts');
 const usersRouter = require('./routes/users');
-
-connect();
+const sdsEventListener = require('./web3/sdsEventListener');
 
 const app = express();
 app.set('port', process.env.PORT || 5051);
@@ -55,5 +56,6 @@ app.use((err, req, res, next) => {
 app.listen(app.get('port'), () => {
   console.log(app.get('port'), 'is up and listening');
 });
+sdsEventListener();
 
 module.exports = app;
---
+++ server/web3/sdsEventListener.js
@@ -0,0 +1,499 @@
+const Web3 = require('web3');
+const axios = require('axios');
+const Nft = require('../schemas/nfts');
+const User = require('../schemas/users');
+// TODO: make it receive variables and wrap it for multiple listener
+const GOERLIWEBSOCKET =
+  'wss://goerli.infura.io/ws/v3/f09f2f4de3164c8eb1a057b84bae7113';
+const CA = '0x456B5b4595D4CEeB43fEd7Fa3066745e6b19eaB2';
+const sdhAddress = '0x7C54f2BC695d540887B0975FEFe36E4a74b66f26';
+const ABI = [
+  {
+    inputs: [],
+    stateMutability: 'nonpayable',
+    type: 'constructor',
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: 'address',
+        name: 'owner',
+        type: 'address',
+      },
+      {
+        indexed: true,
+        internalType: 'address',
+        name: 'approved',
+        type: 'address',
+      },
+      {
+        indexed: true,
+        internalType: 'uint256',
+        name: 'tokenId',
+        type: 'uint256',
+      },
+    ],
+    name: 'Approval',
+    type: 'event',
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: 'address',
+        name: 'owner',
+        type: 'address',
+      },
+      {
+        indexed: true,
+        internalType: 'address',
+        name: 'operator',
+        type: 'address',
+      },
+      {
+        indexed: false,
+        internalType: 'bool',
+        name: 'approved',
+        type: 'bool',
+      },
+    ],
+    name: 'ApprovalForAll',
+    type: 'event',
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: 'address',
+        name: 'previousOwner',
+        type: 'address',
+      },
+      {
+        indexed: true,
+        internalType: 'address',
+        name: 'newOwner',
+        type: 'address',
+      },
+    ],
+    name: 'OwnershipTransferred',
+    type: 'event',
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: 'address',
+        name: 'from',
+        type: 'address',
+      },
+      {
+        indexed: true,
+        internalType: 'address',
+        name: 'to',
+        type: 'address',
+      },
+      {
+        indexed: true,
+        internalType: 'uint256',
+        name: 'tokenId',
+        type: 'uint256',
+      },
+    ],
+    name: 'Transfer',
+    type: 'event',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'address',
+        name: 'to',
+        type: 'address',
+      },
+      {
+        internalType: 'uint256',
+        name: 'tokenId',
+        type: 'uint256',
+      },
+    ],
+    name: 'approve',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'address',
+        name: 'owner',
+        type: 'address',
+      },
+    ],
+    name: 'balanceOf',
+    outputs: [
+      {
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'uint256',
+        name: 'tokenId',
+        type: 'uint256',
+      },
+    ],
+    name: 'getApproved',
+    outputs: [
+      {
+        internalType: 'address',
+        name: '',
+        type: 'address',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'address',
+        name: 'owner',
+        type: 'address',
+      },
+      {
+        internalType: 'address',
+        name: 'operator',
+        type: 'address',
+      },
+    ],
+    name: 'isApprovedForAll',
+    outputs: [
+      {
+        internalType: 'bool',
+        name: '',
+        type: 'bool',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'address',
+        name: 'recipient',
+        type: 'address',
+      },
+      {
+        internalType: 'string',
+        name: 'tokenURI',
+        type: 'string',
+      },
+    ],
+    name: 'mintNFT',
+    outputs: [
+      {
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256',
+      },
+    ],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'name',
+    outputs: [
+      {
+        internalType: 'string',
+        name: '',
+        type: 'string',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'owner',
+    outputs: [
+      {
+        internalType: 'address',
+        name: '',
+        type: 'address',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'uint256',
+        name: 'tokenId',
+        type: 'uint256',
+      },
+    ],
+    name: 'ownerOf',
+    outputs: [
+      {
+        internalType: 'address',
+        name: '',
+        type: 'address',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'renounceOwnership',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'address',
+        name: 'from',
+        type: 'address',
+      },
+      {
+        internalType: 'address',
+        name: 'to',
+        type: 'address',
+      },
+      {
+        internalType: 'uint256',
+        name: 'tokenId',
+        type: 'uint256',
+      },
+    ],
+    name: 'safeTransferFrom',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'address',
+        name: 'from',
+        type: 'address',
+      },
+      {
+        internalType: 'address',
+        name: 'to',
+        type: 'address',
+      },
+      {
+        internalType: 'uint256',
+        name: 'tokenId',
+        type: 'uint256',
+      },
+      {
+        internalType: 'bytes',
+        name: 'data',
+        type: 'bytes',
+      },
+    ],
+    name: 'safeTransferFrom',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'address',
+        name: 'operator',
+        type: 'address',
+      },
+      {
+        internalType: 'bool',
+        name: 'approved',
+        type: 'bool',
+      },
+    ],
+    name: 'setApprovalForAll',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'bytes4',
+        name: 'interfaceId',
+        type: 'bytes4',
+      },
+    ],
+    name: 'supportsInterface',
+    outputs: [
+      {
+        internalType: 'bool',
+        name: '',
+        type: 'bool',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'symbol',
+    outputs: [
+      {
+        internalType: 'string',
+        name: '',
+        type: 'string',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'uint256',
+        name: 'tokenId',
+        type: 'uint256',
+      },
+    ],
+    name: 'tokenURI',
+    outputs: [
+      {
+        internalType: 'string',
+        name: '',
+        type: 'string',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'address',
+        name: 'from',
+        type: 'address',
+      },
+      {
+        internalType: 'address',
+        name: 'to',
+        type: 'address',
+      },
+      {
+        internalType: 'uint256',
+        name: 'tokenId',
+        type: 'uint256',
+      },
+    ],
+    name: 'transferFrom',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'address',
+        name: 'newOwner',
+        type: 'address',
+      },
+    ],
+    name: 'transferOwnership',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+];
+
+const web3 = new Web3(new Web3.providers.WebsocketProvider(GOERLIWEBSOCKET));
+const Contract = new web3.eth.Contract(ABI, CA);
+
+const getTokenURIData = async (tokenURI) => {
+  try {
+    const result = await axios.get(tokenURI);
+    console.log(result.data);
+    return result.data;
+  } catch (err) {
+    return null;
+  }
+};
+
+const updateNft = async (tokenData) => {
+  // 1. 먼저 해당 nft가 db에 존재하는지 확인한다.
+  // 2. 있으면 업데이트 한다.
+  // 3. 없으면 새로 만든다.
+  const { contractAddress, tokenId } = tokenData;
+  let nft = await Nft.findOne({ contractAddress, tokenId });
+  console.log('is nft found?', nft);
+  if (!nft) {
+    console.log('no such nft found');
+    nft = await Nft.create(tokenData);
+  } else {
+    console.log('nft found, update existing one');
+    nft.updateOne({ tokenData }); // TODO: potential bug point, NOT FIXED.
+  }
+};
+
+const updateUser = async () => {};
+const updateContract = async () => {};
+
+module.exports = () => {
+  Contract.events.Transfer().on('data', async (event) => {
+    // 1. query chain for detailed information
+    // extract tokenid from event data
+    const { transactionHash, address, returnValues } = event;
+    const { tokenId, to } = returnValues;
+    // get metadata of minted nft with web3 call
+    const tokenURI = await Contract.methods.tokenURI(tokenId).call({
+      from: '0x7C54f2BC695d540887B0975FEFe36E4a74b66f26', // TODO: control by env
+    });
+
+    const tokenData = await getTokenURIData(tokenURI);
+    tokenData.contractAddress = address;
+    tokenData.tokenId = tokenId;
+    tokenData.transactionHash = transactionHash;
+    tokenData.tokenURI = tokenURI;
+    tokenData.owner = to;
+    tokenData.creator = sdhAddress; // TODO make variable
+
+    let nfts = await Nft.find({});
+    console.log(nfts);
+    // 2. update nft db
+    await updateNft(tokenData);
+    nfts = await Nft.find({});
+    console.log(nfts);
+    // 3. update user db
+    updateUser();
+    // 4. update contract db
+    updateContract();
+    console.log('data set: ');
+    console.log(event);
+    console.log('extracting data ');
+    console.log(event.returnValues);
+  });
+};
+
+// const dbTestFunc = async () => {
+//   const tokenURIData = await getTokenURIData(
+//     'https://sds-721.s3.ap-northeast-2.amazonaws.com/8.json',
+//   );
+//   updateNft(7, CA, tokenURIData);
+// };
+
+// dbTestFunc();

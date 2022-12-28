be7dbb62df1a69e54fcb13a2f1a528c264e14ad2
SeolDonghun <ssalssi1@gmail.com>
2022-12-28 13:32:42 +0900

    developing nft controllers

--- server/controllers/nftsController.js
+++ server/controllers/nftsController.js
@@ -1,6 +1,6 @@
+const fs = require('fs');
 const User = require('../schemas/users');
 const Nft = require('../schemas/nfts');
-const dummyData = require('../dummyData');
 
 module.exports = {
   getAllNfts: async (req, res, next) => {
@@ -57,7 +57,30 @@ module.exports = {
       return next(err);
     }
   },
-  postNft: async (req, res, next) => {
+  mintNft: async (req, res, next) => {
+    // ipfs uri : https://ipfs.io/ipfs/QmdnbubfpnwV9SVH3f2c6rh2q8QFt76AUdbW8W68UoLid9
+    const { contractAddress } = req.params;
+    if (!contractAddress) {
+      return res.status(403).json({
+        status: 'error',
+        message: 'valid contract address must be provided',
+      });
+    }
+    const { name, description, image, attributes } = req.body;
+    if (!name && !description && !image && !attributes) {
+      return res
+        .status(403)
+        .json({ status: 'error', message: 'input all values' });
+    }
+    const uploadMetaData = { name, description, image, attributes };
+    fs.writeFileSync('../test.json', JSON.stringify(uploadMetaData));
+    // 1. req body에서 입력을 받는다.
+    // 1-1. img업로드를 대신해줄수도 있다. 이건 나중에 구현한다.
+    // 2. 정보를 조합해서 json파일로 만든다.
+    // 3. 받은 정보를 s3에 업로드한다.
+    // 4. s3에 업로드된 uri를 가져온다
+    // 5. 해당 uri로 web3 mint를 호출한다.
+    // 6. 결과를 반환한다.
     try {
       const { account } = req.body;
       let foundAccount = await User.findOne({ account });
